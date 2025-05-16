8b70c164986b29c7413918670e946b2403637bd8
femarko <fenstermark7@gmail.com>
2025-05-16 18:22:46 +0200

    feat: quantity of positional args check added into deco.

--- task1/deco.py
+++ task1/deco.py
@@ -7,6 +7,8 @@ def strict(func: FunctionType) -> Callable[..., Any]:
         func_annotations: Dict[str, type] = func.__annotations__.copy()  # typing.Dict - for backward compatibility
         func_annotations.pop('return', None)
         param_names: list[str] = list(func_annotations)
+        if len(args) > len(param_names):
+            raise TypeError(f"Only {len(param_names)} positional arguments are allowed, got {len(args)}.")
         params_with_names: Dict[str, Any] = dict(zip(param_names, args))
         extra_kwargs: set[str] = set(kwargs) & set(params_with_names)
         if extra_kwargs:
@@ -15,7 +17,7 @@ def strict(func: FunctionType) -> Callable[..., Any]:
         for param_name, param_type in func_annotations.items():
             if not param_name in params_with_names:
                 raise TypeError(f"Missing argument {param_name}.")
-            if type(params_with_names[param_name]) is not param_type:
+            if type(params_with_names[param_name]) != param_type:
                 raise TypeError(
                     f"{param_name} must be of type {param_type.__name__}, "
                     f"got {type(params_with_names[param_name]).__name__}."
